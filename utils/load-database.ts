c48afe7b10f583459b15ab5ddcbb0b32b3f7f2ef
Joseph Herrera <josephherreraleon@gmail.com>
2024-07-22 09:54:54 -0600

    Refactor Select statements

--- utils/load-database.ts
+++ utils/load-database.ts
@@ -19,12 +19,12 @@ export const loadDatabase = async () => {
       const fileInfo = await FileSystem.getInfoAsync(dbPath);
       if (fileInfo.exists) {
         await FileSystem.deleteAsync(dbPath);
-        console.log('Database deleted successfully.');
+        console.log("Database deleted successfully.");
       } else {
-        console.log('Database file does not exist.');
+        console.log("Database file does not exist.");
       }
     } catch (error) {
-      console.error('Error deleting database:', error);
+      console.error("Error deleting database:", error);
     }
   };
 
@@ -46,7 +46,7 @@ export const loadDatabase = async () => {
         creation_date TEXT NOT NULL
       );
     `;
-  
+
     const createTableStudents = `
       CREATE TABLE IF NOT EXISTS Students (
         id_student INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -59,7 +59,7 @@ export const loadDatabase = async () => {
         FOREIGN KEY (id_user) REFERENCES Users(id_user)
       );
     `;
-  
+
     const createTableTeachers = `
       CREATE TABLE IF NOT EXISTS Teachers (
         id_teacher INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -68,7 +68,7 @@ export const loadDatabase = async () => {
         FOREIGN KEY (id_user) REFERENCES Users(id_user)
       );
     `;
-  
+
     const createTableClasses = `
       CREATE TABLE IF NOT EXISTS Classes (
         id_class INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -79,7 +79,7 @@ export const loadDatabase = async () => {
         FOREIGN KEY (id_teacher) REFERENCES Teachers(id_teacher)
       );
     `;
-  
+
     const createTableAssists = `
       CREATE TABLE IF NOT EXISTS Assists (
         id_attendance INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -91,7 +91,7 @@ export const loadDatabase = async () => {
         FOREIGN KEY (id_class) REFERENCES Classes(id_class)
       );
     `;
-  
+
     const createTableGrades = `
       CREATE TABLE IF NOT EXISTS Grades (
         id_grade INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -102,16 +102,16 @@ export const loadDatabase = async () => {
         FOREIGN KEY (id_class) REFERENCES Classes(id_class)
       );
     `;
-  
+
     const queries = [
       createTableUsersQuery,
       createTableStudents,
       createTableTeachers,
       createTableClasses,
       createTableAssists,
-      createTableGrades
+      createTableGrades,
     ];
-  
+
     for (const query of queries) {
       try {
         db.execAsync(query);
@@ -129,40 +129,40 @@ export const loadDatabase = async () => {
     (2, 'Joseph Herrera Leon', '321', 'student@uppenjamo.com', '01-01-2045');
   `;
 
-  const insertStudentsDataQuery = `
+    const insertStudentsDataQuery = `
     INSERT OR IGNORE INTO Students (id_student, id_user, generation, group_index, grade, major, note) 
     VALUES (1, 2, '2020', 'A', '9', 'Ingenieria en Software', 'N/A');
   `;
 
-  const insertTeachersDataQuery = `
+    const insertTeachersDataQuery = `
     INSERT OR IGNORE INTO Teachers (id_teacher, field, id_user) 
     VALUES (1, 'Ingenieria en Software', 1);
   `;
 
-  const insertClassesDataQuery = `
+    const insertClassesDataQuery = `
     INSERT OR IGNORE INTO Classes (id_class, name, code, cover, id_teacher) 
     VALUES (1, 'Programacion Moviles II', '69420', '', 1);
   `;
 
-  const insertAssistsDataQuery = `
+    const insertAssistsDataQuery = `
     INSERT OR IGNORE INTO Assists (id_attendance, absent, date, id_student, id_class) 
     VALUES (1, 1, '01-03-2024', 1, 1);
   `;
 
-  const insertGradesDataQuery = `
+    const insertGradesDataQuery = `
     INSERT OR IGNORE INTO Grades (id_grade, grade, id_student, id_class) 
     VALUES (1, 9.8, 1, 1);
   `;
-  
+
     const queries = [
       insertUsersDataQuery,
       insertStudentsDataQuery,
       insertTeachersDataQuery,
       insertClassesDataQuery,
       insertAssistsDataQuery,
-      insertGradesDataQuery
+      insertGradesDataQuery,
     ];
-  
+
     for (const query of queries) {
       try {
         db.execAsync(query);
@@ -179,16 +179,16 @@ export const loadDatabase = async () => {
     const deleteClassesDataQuery = `DELETE FROM Classes;`;
     const deleteAssistsDataQuery = `DELETE FROM Assists;`;
     const deleteGradesDataQuery = `DELETE FROM Grades;`;
-  
+
     const queries = [
       deleteUsersDataQuery,
       deleteStudentsDataQuery,
       deleteTeachersDataQuery,
       deleteClassesDataQuery,
       deleteAssistsDataQuery,
-      deleteGradesDataQuery
+      deleteGradesDataQuery,
     ];
-  
+
     for (const query of queries) {
       try {
         db.execAsync(query);
@@ -196,7 +196,7 @@ export const loadDatabase = async () => {
         console.error("Error deleting data:", error);
       }
     }
-  
+
     try {
       db.execAsync(`VACUUM;`);
     } catch (error) {
@@ -206,11 +206,11 @@ export const loadDatabase = async () => {
 
   const init = () => {
     createTables();
-    insertData(); 
+    insertData();
     //dropDatabase();
-    //deleteAllData(); 
+    //deleteAllData();
   };
-  
+
   init();
 
   interface User {
@@ -258,32 +258,33 @@ export const loadDatabase = async () => {
     grade: number;
   }
 
-  const allUsersRows = (await db.getAllAsync("SELECT * FROM Users")) as any;
-  for (const row of allUsersRows) {
+  const allUsersRows = await db.getAllAsync("SELECT * FROM Users");
+  const users = allUsersRows as User[];
+  for (const row of users) {
     console.log(row.id_user, row.name, row.password, row.creation_date);
   }
 
-  const allStudentsRows = (await db.getAllAsync(
-    "SELECT * FROM Students"
-  )) as any;
-  for (const row of allStudentsRows) {
+  const allStudentsRows = await db.getAllAsync("SELECT * FROM Students");
+  const students = allStudentsRows as Students[];
+  for (const row of students) {
     console.log(row.id_user, row.generation, row.grade, row.major, row.note);
   }
 
-  const allTeachersRows = (await db.getAllAsync(
-    "SELECT * FROM Teachers"
-  )) as any;
-  for (const row of allTeachersRows) {
+  const allTeachersRows = await db.getAllAsync("SELECT * FROM Teachers");
+  const teachers = allTeachersRows as Teachers[];
+  for (const row of teachers) {
     console.log(row.id_teacher, row.id_user, row.field);
   }
 
-  const allClassesRows = (await db.getAllAsync("SELECT * FROM Classes")) as any;
-  for (const row of allClassesRows) {
+  const allClassesRows = await db.getAllAsync("SELECT * FROM Classes");
+  const classes = allClassesRows as Classes[];
+  for (const row of classes) {
     console.log(row.id_class, row.id_teacher, row.name, row.code, row.cover);
   }
 
-  const allAssistsRows = (await db.getAllAsync("SELECT * FROM Assists")) as any;
-  for (const row of allAssistsRows) {
+  const allAssistsRows = await db.getAllAsync("SELECT * FROM Assists");
+  const assists = allAssistsRows as Assists[];
+  for (const row of assists) {
     console.log(
       row.id_attendance,
       row.id_student,
@@ -293,8 +294,9 @@ export const loadDatabase = async () => {
     );
   }
 
-  const allGradesRows = (await db.getAllAsync("SELECT * FROM Grades")) as any;
-  for (const row of allGradesRows) {
+  const allGradesRows = await db.getAllAsync("SELECT * FROM Grades");
+  const grades = allGradesRows as Grades[];
+  for (const row of grades) {
     console.log(row.id_grade, row.id_student, row.id_class, row.grade);
   }
 
