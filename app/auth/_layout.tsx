c4cc6ea77f61c92e779c8fbe5105a8600e4c814f
Joseph Herrera <josephherreraleon@gmail.com>
2024-07-29 11:07:46 -0600

    Add return from sign In to sign up

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -2,25 +2,24 @@ import React, { useState } from 'react';
 import SignUp from '@/components/SignUp';
 import SignIn from '@/components/SignIn';
 
-interface SignUpProps {
+interface AuthNavigatorProps {
     setIsSignedUp: React.Dispatch<React.SetStateAction<boolean>>;
     setFirstTimeRegister: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function AuthNavigator({ setIsSignedUp, setFirstTimeRegister }: SignUpProps) {
+export default function AuthNavigator({ setIsSignedUp, setFirstTimeRegister }: AuthNavigatorProps) {
     const [firstTimeRegister, setFirstTimeRegisterInternal] = useState<boolean>(true);
 
-    const handleChangePage = () => {
-        setFirstTimeRegisterInternal(false);
+    console.log(firstTimeRegister);
+
+    const handleChangePage = (isRegistering: boolean) => {
+        setFirstTimeRegisterInternal(isRegistering);
     };
 
-    if (firstTimeRegister) {
-        return <SignUp setIsSignedUp={setIsSignedUp} setFirstTimeRegister={handleChangePage} />;
-    }
-    else{
-        return (
-            <SignIn setIsSignedUp={setIsSignedUp} setFirstTimeRegister={handleChangePage}/>
-        );
-    }
-    
+    return firstTimeRegister ? (
+        <SignUp setIsSignedUp={setIsSignedUp} setFirstTimeRegister={() => handleChangePage(false)} />
+    ) : (
+        <SignIn setIsSignedUp={setIsSignedUp} setFirstTimeRegister={() => handleChangePage(true)} />
+    );
 }
+
