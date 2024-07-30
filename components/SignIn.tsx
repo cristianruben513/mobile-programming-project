d1ae939956a6866ff0acff02d2ade118bbc394b1
Joseph Herrera <josephherreraleon@gmail.com>
2024-07-29 19:07:50 -0600

    Update sign in style

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { Text, TextInput, TouchableOpacity, SafeAreaView, useColorScheme } from "react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import * as SQLite from 'expo-sqlite';
 import { config } from "@/config/config";
+import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from "@/components/ThemedText";
 
 const db = SQLite.openDatabaseSync(config.DATABASE_NAME);
 
@@ -48,6 +50,10 @@ export default function SignIn({ setIsSignedUp, setFirstTimeRegister }: SignUpPr
         setFirstTimeRegister(true);
     };
 
+    const colorScheme = useColorScheme();
+
+    const textColor = colorScheme === 'dark' ? 'text-white' : 'text-black';
+
     return (
         <Formik
             initialValues={{
@@ -65,53 +71,52 @@ export default function SignIn({ setIsSignedUp, setFirstTimeRegister }: SignUpPr
                 errors,
                 touched,
             }) => (
-                <View style={styles.container}>
-                    <Text>Email</Text>
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={handleChange("email")}
-                        onBlur={handleBlur("email")}
-                        value={values.email}
-                    />
-                    {errors.email && touched.email ? (
-                        <Text style={styles.errorText}>{errors.email}</Text>
-                    ) : null}
+                <SafeAreaView className="flex-1 h-full">
+                    <ThemedView className="flex-1 justify-center h-full p-6">
+                        <ThemedText type="title" style={{ fontSize: 30, marginBottom: 10 }}>Sign In</ThemedText>
+                        <ThemedText>Email</ThemedText>
+                        <TextInput
+                            className="text-white border border-neutral-400 rounded-lg p-6 py-4 mb-6"
+                            onChangeText={handleChange("email")}
+                            onBlur={handleBlur("email")}
+                            value={values.email}
+                            style={
+                                { color: colorScheme === 'dark' ? 'white' : 'black' }
+                            }
+                        />
+                        {errors.email && touched.email ? (
+                            <Text className="text-red-500 mb-6">{errors.email}</Text>
+                        ) : null}
 
-                    <Text>Password</Text>
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={handleChange("password")}
-                        onBlur={handleBlur("password")}
-                        value={values.password}
-                        secureTextEntry
-                    />
-                    {errors.password && touched.password ? (
-                        <Text style={styles.errorText}>{errors.password}</Text>
-                    ) : null}
+                        <ThemedText>Password</ThemedText>
+                        <TextInput
+                            className="text-white border border-neutral-400 rounded-lg p-6 py-4  mb-6"
+                            onChangeText={handleChange("password")}
+                            onBlur={handleBlur("password")}
+                            value={values.password}
+                            secureTextEntry
+                        />
+                        {errors.password && touched.password ? (
+                            <Text className="text-red-500 mb-6">{errors.password}</Text>
+                        ) : null}
 
-                    <Button onPress={() => handleSubmit()} title="Sign In" />
-                    <Button onPress={() => changePage()} title="Don't have an account? Sign Up"/>
-                </View>
+                        <TouchableOpacity
+                            className="bg-green-600 mb-8 rounded-xl py-4 px-6 w-full items-center justify-center"
+                            onPress={() => handleSubmit}
+                        >
+                            <Text className="text-white font-bold">Sign In</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            className="bg-blue-700 rounded-xl py-4 px-6 w-full items-center justify-center"
+                            onPress={changePage}
+                        >
+                            <Text className="text-white font-bold">Don't have an account? Sign Up</Text>
+                        </TouchableOpacity>
+                    </ThemedView>
+                </SafeAreaView>
+
             )}
         </Formik>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        padding: 16,
-    },
-    input: {
-        height: 40,
-        borderColor: "gray",
-        borderWidth: 1,
-        marginBottom: 12,
-        paddingHorizontal: 8,
-    },
-    errorText: {
-        color: "red",
-        marginBottom: 12,
-    },
-});
