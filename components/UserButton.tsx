36ecd62b445b9c05461340d5d09a63e8b97d3c3c
Cristian Ruben <figueroac513@gmail.com>
2024-07-30 18:18:12 -0600

    update user button with her initials

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -1,8 +1,20 @@
-import { Link } from "expo-router";
-import { Image, Pressable } from "react-native";
 import { Colors } from "@/constants/Colors";
+import { useUserStore } from "@/stores/useUserStore";
+import { Link } from "expo-router";
+import { Pressable, Text } from "react-native";
 
 export default function UserButton() {
+  const { user } = useUserStore();
+
+  const { userName } = user;
+
+  const initials = userName
+    .split(" ")
+    .map((word: string) => word[0])
+    .join("")
+    .slice(0, 2)
+    .toUpperCase();
+
   return (
     <Link href="/user" asChild>
       <Pressable
@@ -11,12 +23,13 @@ export default function UserButton() {
           borderRadius: 20,
           marginRight: 20,
           padding: 2,
+          width: 30,
+          height: 30,
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <Image
-          source={require("@/assets/images/react-logo.png")}
-          style={{ width: 25, height: 25 }}
-        />
+        <Text className="text-white text-sm">{initials}</Text>
       </Pressable>
     </Link>
   );
